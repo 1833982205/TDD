d7454e51f38d763296e60b6cb6ea3e1932f1229d
1833982205 <1833982205@qq.com>
2020-05-26 22:22:07 +0800

    0526 new URL + view for adding to existing lists.

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -72,6 +72,7 @@ class NewVisitorTest(LiveServerTestCase): #(1)
         #She visits that URL - her to-do list is still there.
 
         #Satisfied, she goes back to sleep
+        
     def test_multiple_users_can_start_lists_at_different_urls(self):
         # Edith starts a new to-do list
         self.browser.get(self.live_server_url)
@@ -102,12 +103,12 @@ class NewVisitorTest(LiveServerTestCase): #(1)
         inputbox = self.browser.find_element_by_id('id_new_item')
         inputbox.send_keys('Buy milk')
         inputbox.send_keys(Keys.ENTER)
-        self.wait_for_row_in_list_table('1:Buy milk')
+        self.wait_for_row_in_list_table('1: Buy milk')
 
         #Francis gets his own unique URL
-        frantics_list_url = self.browser.current_url
-        self.assertRegex(frantics_list_url,'/lists/.+')
-        self.assertNotEqual(frantics_list_url, edith_list_url)
+        francis_list_url = self.browser.current_url
+        self.assertRegex(francis_list_url,'/lists/.+')
+        self.assertNotEqual(francis_list_url, edith_list_url)
 
         #Again, there is no trace of Edith's list
         page_text = self.browser.find_element_by_tag_name('body').text
